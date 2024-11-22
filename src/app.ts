40359dd8baaa947fe5b0f10a9e13b017d4f1f77b
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-22 18:42:55 +0600

    adding project

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const port = 3000;
+
+// parser
+app.use(express.json());
+app.use(cors())
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
